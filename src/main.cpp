b5ccc13cf6f0bf1ba119fc05790609362c93af7c
Vishay Agarwal <v.agrwl17@gmail.com>
2026-02-23 01:53:31 +0000

    compression of tokens

--- src/main.cpp
+++ src/main.cpp
@@ -26,3 +26,35 @@ double computeImportance(const std::string& text, int recencyIndex) {
     double recencyWeight = std::exp(-0.15 * recencyIndex);
     return entropyProxy * recencyWeight;
 }
+
+std::vector<Message> compressContext(
+    std::vector<std::string> messages,
+    int tokenBudget
+) {
+    std::vector<Message> scored;
+
+    for (int i = 0; i < messages.size(); ++i) {
+        int tokenEstimate = messages[i].length() / 4;
+        double importance = computeImportance(messages[i], i);
+        double density = importance / std::max(tokenEstimate, 1);
+
+        scored.push_back({messages[i], tokenEstimate, importance, density});
+    }
+
+    std::sort(scored.begin(), scored.end(),
+        [](const Message& a, const Message& b) {
+            return a.density > b.density;
+        });
+
+    std::vector<Message> selected;
+    int usedTokens = 0;
+
+    for (auto& msg : scored) {
+        if (usedTokens + msg.tokens <= tokenBudget) {
+            selected.push_back(msg);
+            usedTokens += msg.tokens;
+        }
+    }
+
+    return selected;
+}
