8fe63f3bf17f624be88166ca3bc4e25328188ac4
Vishay Agarwal <v.agrwl17@gmail.com>
2026-02-23 01:52:19 +0000

    intialize

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ArchLLM-Lab
+-----------
+Objective:
+Maximize retained semantic information under strict token budget.
+
+Formalized as:
+Max sum(v_i * x_i)
+Subject to sum(t_i * x_i) <= B
+
+Where:
+v_i = information value
+t_i = token cost
+B   = context window constraint
+*/
+
